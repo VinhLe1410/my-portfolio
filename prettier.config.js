9e85adae7814a800faa698fba4e9a6ca289c7c9b
Phuc Vinh Le <lpvinh2k4@gmail.com>
2025-06-16 23:44:00 +1000

    Chore: Added Prettier and ESLint config

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: true, // semi-colons after every code line
+  singleQuote: true, // strictly single quote
+  trailingComma: 'es5', // trailing commas where valid in ES5 (I'll figure out later)
+  tabWidth: 2,
+  printWidth: 80,
+};
