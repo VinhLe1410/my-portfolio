7ccb8c5ff4af83e8f81cd320d2c8bff51a2490dc
Phuc Vinh Le <lpvinh2k4@gmail.com>
2025-06-28 04:49:00 +1000

    Feat: Added Navigation Bar base

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from '../components/header'
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
@@ -13,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: 'Welcome Page',
+  title: 'Portfolio - Vinh Le',
   description: 'Welcome to my Portfolio Page',
 };
 
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
         {children}
       </body>
     </html>
