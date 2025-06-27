ca1aeaf66bc3c9ef3f789114a55501276902ea6c
Phuc Vinh Le <lpvinh2k4@gmail.com>
2025-06-28 04:51:35 +1000

    Feat: Added Navigation Bar base

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,18 @@
-'use client'
+'use client';
 
-import React from "react"
-import { motion } from "motion/react"
+import React from 'react';
+import { motion } from 'motion/react';
 
 export default function Header() {
-    return <header className="z-[999] relative">
-        <motion.div className="fixed top-0 h-[4.5rem] w-full rounded-none 
-                                border border-white/20 bg-white/25 
-                                shadow-lg backdrop-blur-2xl"
-            initial={{ y: -100, opacity: 0}}
-            animate={{ y: 0, opacity: 1}}>
-        </motion.div>
+  return (
+    <header className="z-[999] relative">
+      <motion.div
+        className="fixed top-0 h-[4.5rem] w-full rounded-none 
+                    border border-white/20 bg-white/25 
+                    shadow-lg backdrop-blur-2xl"
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+      ></motion.div>
     </header>
+  );
 }
